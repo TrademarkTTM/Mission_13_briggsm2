efef29be574015e0a3d2e1b17fa1e1a50381bf88
Mark Briggs <markbriggs321@gmail.com>
2023-03-29 14:01:24 -0600

    Nav Bar Update

--- frontend/src/NavBar.tsx
+++ frontend/src/NavBar.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 function NavBar() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="navbar-collapse" id="navbarNav">
           <a className="navbar-brand">Joel's Movie List</a>
           <ul className="navbar-nav">
